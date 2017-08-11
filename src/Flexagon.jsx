648a9860d51c3cd13bbcb2230b7df50db1ab6680
jtkaczyszyn <jtkaczyszyn@elementum.com>
2017-08-11 17:59:17 -0300

    diagonals

--- src/Flexagon.jsx
+++ src/Flexagon.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 const styles = {
   line: {
-    stroke:'#000000',
-    strokeWidth:4
+    stroke:'#f0f0f0',
+    strokeWidth:1
   },
   frame: {
-    stroke:'#000000',
-    strokeWidth:4,
+    stroke:'#f0f0f0',
+    strokeWidth:1,
     fill: 'none'
   },
   svg:{
-    margin: '250px'
+    margin: '0px'
   }
 }
 
@@ -19,44 +19,66 @@ const toRadians = angle => angle * (Math.PI / 180);
 const imageWidth = 300;
 const imageHeigth = imageWidth/4 + Math.sqrt(Math.pow(imageWidth,2) - Math.pow(imageWidth/2,2));
 const sideLength = (Math.sqrt(5)/4)*imageWidth;
-const width = 3.5 * imageWidth;
+const width = 4 * imageWidth;
 const height = imageWidth;
 
 const a1 = {
-  x: (3/7) * width,
+  x: (4/8) * width,
   y: height/4
 }
 
 const a2 = {
-  x: (1/7) * width,
+  x: (2/8) * width,
   y: height/4
 }
 
 const a3 = {
-  x: (5/7) * width,
+  x: (6/8) * width,
   y: height/4
 }
 
 
-const lines = [
+const innerLines = [
   // vertical
-  { x1: (1/7) * width, y1:        0, x2: (1/7) * width, y2:   height },
+  { x1: (0/8) * width, y1: 0,                  x2: (0/8) * width, y2: 2 * height         },
+  { x1: (1/8) * width, y1: 0,                  x2: (1/8) * width, y2: 2 * height         },
+  { x1: (2/8) * width, y1: 0,                  x2: (2/8) * width, y2: 2 * height         },
+  { x1: (3/8) * width, y1: 0,                  x2: (3/8) * width, y2: 2 * height         },
+  { x1: (4/8) * width, y1: 0,                  x2: (4/8) * width, y2: 2 * height         },
+  { x1: (5/8) * width, y1: 0,                  x2: (5/8) * width, y2: 2 * height         },
+  { x1: (6/8) * width, y1: 0,                  x2: (6/8) * width, y2: 2 * height         },
+  { x1: (7/8) * width, y1: 0,                  x2: (7/8) * width, y2: 2 * height         },
+  { x1: (8/8) * width, y1: 0,                  x2: (8/8) * width, y2: 2 * height         },
+
+  // horizontal
+  { x1: 0,             y1: (0/4) * 2 * height, x2: 2 * width,     y2: (0/4) * 2 * height },
+  { x1: 0,             y1: (1/4) * 2 * height, x2: 2 * width,     y2: (1/4) * 2 * height },
+  { x1: 0,             y1: (2/4) * 2 * height, x2: 2 * width,     y2: (2/4) * 2 * height },
+  { x1: 0,             y1: (3/4) * 2 * height, x2: 2 * width,     y2: (3/4) * 2 * height },
+  { x1: 0,             y1: (4/4) * 2 * height, x2: 2 * width,     y2: (4/4) * 2 * height },
 
   // up diagonals
-  { x1: (1/7) * width, y1: height/2, x2: (3/7) * width, y2:        0 },
-  { x1: (1/7) * width, y1:   height, x2: (5/7) * width, y2:        0 },
-  { x1: (3/7) * width, y1:   height, x2:         width, y2:        0 },
-  { x1: (5/7) * width, y1:   height, x2:         width, y2: height/2 },
+  { x1: (0/8) * width, y1: (1/4) * 2 * height, x2: (2/8) * width, y2: 0                  },
+  { x1: (0/8) * width, y1: (2/4) * 2 * height, x2: (4/8) * width, y2: 0                  },
+  { x1: (0/8) * width, y1: (3/4) * 2 * height, x2: (6/8) * width, y2: 0                  },
+  { x1: (0/8) * width, y1: (4/4) * 2 * height, x2: (8/8) * width, y2: 0                  },
+  { x1: (2/8) * width, y1: (4/4) * 2 * height, x2: (8/8) * width, y2: (1/4) * 2 * height },
+  { x1: (4/8) * width, y1: (4/4) * 2 * height, x2: (8/8) * width, y2: (2/4) * 2 * height },
+  { x1: (6/8) * width, y1: (4/4) * 2 * height, x2: (8/8) * width, y2: (3/4) * 2 * height },
 
   // down diagonals
-  { x1: (1/7) * width, y1: height/2, x2: (3/7) * width, y2:   height },
-  { x1: (1/7) * width, y1:        0, x2: (5/7) * width, y2:   height },
-  { x1: (3/7) * width, y1:        0, x2:         width, y2:   height },
-  { x1: (5/7) * width, y1:        0, x2:         width, y2: height/2 }
+  { x1: (0/8) * width, y1: (3/4) * 2 * height, x2: (2/8) * width, y2: (4/4) * 2 * height },
+  { x1: (0/8) * width, y1: (2/4) * 2 * height, x2: (4/8) * width, y2: (4/4) * 2 * height },
+  { x1: (0/8) * width, y1: (1/4) * 2 * height, x2: (6/8) * width, y2: (4/4) * 2 * height },
+  { x1: (0/8) * width, y1: (0/4) * 2 * height, x2: (8/8) * width, y2: (4/4) * 2 * height },
+  { x1: (2/8) * width, y1: (0/4) * 2 * height, x2: (8/8) * width, y2: (3/4) * 2 * height },
+  { x1: (4/8) * width, y1: (0/4) * 2 * height, x2: (8/8) * width, y2: (2/4) * 2 * height },
+  { x1: (6/8) * width, y1: (0/4) * 2 * height, x2: (8/8) * width, y2: (1/4) * 2 * height },
+
 ]
 
 const Flexagon = ({ rotation, x, y }) => (
-  <svg style={styles.svg} width={width} height={height}>
+  <svg style={styles.svg} width={width} height={2 * height}>
 
     <defs>
       <pattern id="a" height="100%" width="100%" patternContentUnits="objectBoundingBox">
@@ -88,23 +110,25 @@ const Flexagon = ({ rotation, x, y }) => (
 
     <g style={styles.line}>
       {
-        lines.map(({ x1, y1, x2, y2 }, index) => (
+        innerLines.map(({ x1, y1, x2, y2 }, index) => (
           <line key={index} x1={x1} y1={y1} x2={x2} y2={y2} />
         ))
       }
     </g>
-    <rect style={styles.frame} x="0" y="0" width={width} height={height}/>
 
-    
-    <g transform={`translate(${a1.x} ${a1.y}) rotate(0)`}>
-      <rect fill="url(#a)" height={imageHeigth} width={imageWidth} style={{clipPath: 'url(#a1)'}} />
+    <g transform={`translate(0 ${height / 2})`}>
+
+      <g transform={`translate(${a1.x} ${a1.y}) rotate(0)`}>
+        <rect fill="url(#a)" height={imageHeigth} width={imageWidth} style={{clipPath: 'url(#a1)'}} />
+      </g>
+      <g transform={`translate(${a2.x + imageWidth} ${a2.y}) rotate(120 0 ${imageWidth/4})`}>
+        <rect fill="url(#a)" height={imageHeigth} width={imageWidth} style={{clipPath: 'url(#a2)'}} />
+      </g>
+      <g transform={`translate(${a3.x - imageWidth} ${a3.y}) rotate(-120 ${imageWidth} ${imageWidth/4})`}>
+        <rect fill="url(#a)" height={imageHeigth} width={imageWidth} style={{clipPath: 'url(#a3)'}} />
+      </g>
     </g>
-    <g transform={`translate(${a2.x + imageWidth} ${a2.y}) rotate(120 0 ${imageWidth/4})`}>
-      <rect fill="url(#a)" height={imageHeigth} width={imageWidth} style={{clipPath: 'url(#a2)'}} />
-    </g>
-    <g transform={`translate(${a3.x - imageWidth} ${a3.y}) rotate(-120 ${imageWidth} ${imageWidth/4})`}>
-      <rect fill="url(#a)" height={imageHeigth} width={imageWidth} style={{clipPath: 'url(#a3)'}} />
-    </g>
+
   </svg>
 );
 
